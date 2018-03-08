fac3dd854bf5e8d3fa80b41a16287a4f3b5e6a19
kawa <katarzyna.walenko@21s.pl>
2018-03-08 14:27:19 +0100

    #1: intro animation

--- _js/main.js
+++ _js/main.js
@@ -1,2 +1,10 @@
-console.log('jest bundle, ale czy się zmienia?')
-console.log('zmienia się, zmienia')
+import runLogoAnimation from './runLogoAnimation';
+
+$(document).ready(() => {
+
+    runLogoAnimation();
+    // moveBcgLayer($('.js-intro-bcg-shapes'), $('#dev-bcg-intro-color #bcg-1'), 20);
+    // moveBcgLayer($('.js-intro-bcg-shapes'), $('#dev-bcg-intro-color #bcg-2'), 20);
+    // moveBcgLayer($('.js-intro-bcg-shapes'), $('#dev-bcg-intro-color #bcg-3'), 30);
+    console.log('animacja działa? działa!');
+});
--- js_dist/main.bundle.js
+++ js_dist/main.bundle.js
@@ -65,13 +65,57 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__runLogoAnimation__ = __webpack_require__(1);
 
 
-console.log('jest bundle, ale czy się zmienia?');
-console.log('zmienia się, zmienia');
+$(document).ready(() => {
+
+    Object(__WEBPACK_IMPORTED_MODULE_0__runLogoAnimation__["a" /* default */])();
+    // moveBcgLayer($('.js-intro-bcg-shapes'), $('#dev-bcg-intro-color #bcg-1'), 20);
+    // moveBcgLayer($('.js-intro-bcg-shapes'), $('#dev-bcg-intro-color #bcg-2'), 20);
+    // moveBcgLayer($('.js-intro-bcg-shapes'), $('#dev-bcg-intro-color #bcg-3'), 30);
+    console.log('animacja działa? działa!');
+});
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (immutable) */ __webpack_exports__["a"] = runLogoAnimation;
+function runLogoAnimation() {
+
+    $('.js-header, .js-logo-full').show().removeClass('hidden');
+
+    // GSAP vars https://greensock.com/docs/#/HTML5/GSAP/TimelineLite/
+    var tl = new TimelineLite();
+
+    //---------------
+    // LOGO ANIMATION
+    //---------------
+    // logo vars
+    var $introHeader = $('.js-header');
+    var $uDream = $('.js-intro-h11');
+    var $uPlan = $('.js-intro-h12');
+    var $uMaster = $('.js-intro-h13');
+    var $comingSoon = $('.js-intro-h2');
+    var $logoFull = $('.js-logo-full');
+    var $logoPart = $('.js-logo-full > *');
+    var $wheelPart1 = $('.js-wheel-part-1');
+    var $wheelPart2 = $('.js-wheel-part-2');
+    var $wheelPart3 = $('.js-wheel-part-3');
+    var $wheelPart4 = $('.js-wheel-part-4');
+    var $wheelPart5 = $('.js-wheel-part-5');
+    var $logoName = $('.js-logo-text');
+
+    tl.to($introHeader, 0, { opacity: 0 }).to($logoFull, 0, { opacity: 0 }).to($logoPart, 0, { opacity: 0 }).to($comingSoon, 0, { opacity: 0 }).to($uDream, 0, { x: '-=20' }, '+=.5').to($uPlan, 0, { x: '-=20' }).to($uMaster, 0, { x: '-=20' }).to($uDream, 1, { opacity: 1, x: '0', ease: Linear.easeOut }).to($uPlan, 1, { opacity: 1, x: '0', ease: Linear.easeOut }).to($uMaster, 1, { opacity: 1, x: '0', ease: Linear.easeOut }).to($logoFull, 0, { opacity: 1 }).to($wheelPart1, .2, { opacity: 1, ease: Back.easeOut.config(1.4) }, '+=.1').to($wheelPart2, .2, { opacity: 1, ease: Back.easeOut.config(1.4) }, '-=.1').to($wheelPart3, .2, { opacity: 1, ease: Back.easeOut.config(1.4) }, '-=.1').to($wheelPart4, .2, { opacity: 1, ease: Back.easeOut.config(1.4) }, '-=.1').to($wheelPart5, .2, { opacity: 1, ease: Back.easeOut.config(1.4) }, '-=.1').to($logoName, 2, { opacity: 1, ease: Linear.easeOut }, '+=.2').to($comingSoon, 1, { opacity: 1, ease: Linear.easeOut });
+
+    //.to($('#bcg-3-float-3'), 100, {x:'+=1000', y:'+=500'}, '-=.2')
+};
 
 /***/ })
 /******/ ]);
